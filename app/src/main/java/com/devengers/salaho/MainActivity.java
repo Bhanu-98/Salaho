88cd75d98896beedef2c87c6d1347e33743c691e
Bhanu Prakash Oleti <bhanuoleti98@gmail.com>
2020-08-02 16:21:11 +0530

    Modified Nav Menu Items

--- app/src/main/java/com/devengers/salaho/MainActivity.java
+++ app/src/main/java/com/devengers/salaho/MainActivity.java
@@ -20,6 +20,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_add_friend, R.id.nav_join_group)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -45,9 +47,10 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 Log.d("Selected menu %s", String.valueOf(menuItem.getItemId()));
                 switch (menuItem.getItemId()) {
-                    case R.id.nav_gallery:
+                    case R.id.nav_add_friend:
                     case R.id.nav_home:
-                    case R.id.nav_slideshow:
+                    case R.id.nav_join_group:
+                    case R.id.nav_logout:
                 }
                 return false;
             }
